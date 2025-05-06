2b6b46df838e0ab2863701e8d8b4a7133e2d1a4a
husainb0112 <husainb@nycstudents.net>
2025-05-06 02:19:17 +0000

    v1

---
+++ Textbook.java
@@ -0,0 +1,12 @@
+public class Textbook extends Book {
+    private int edition;
+
+    public Textbook(String title, double price, int edition) {
+        super(title, price); 
+        this.edition = edition;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+}
